588c82785b578dea49f30f56381909adff65efea
Matt <mrmuszynski@gmail.com>
2018-02-22 13:20:03 -0700

    Final commit of homwork 2 script

--- homework/APPM5590hw2.py
+++ homework/APPM5590hw2.py
@@ -13,6 +13,7 @@ from os import sys
 sys.path.insert(0, '../../lib')
 from APPM5590 import MLR
 import matplotlib.pyplot as plt
+from numpy import array
 ###############################################################################
 #
 # RABE 3.3
@@ -25,7 +26,7 @@ exams.dataFile = '../data/P076.txt'
 exams.regress()
 exams.partialRegress([1])
 exams.partialRegress([2])
-pdb.set_trace()
+
 #Part A. 
 #Make all the plots we want to have to show we did the job
 exams.scatterAllPartials()
@@ -51,6 +52,18 @@ print('Model 3 rSq: ' + str(exams.rSq))
 print('Model 1 SSE: ' + str(exams.partialRegressions[0].SSE))
 print('Model 2 SSE: ' + str(exams.partialRegressions[1].SSE))
 print('Model 3 SSE: ' + str(exams.SSE))
+print('Model 1 Prediction:')
+x0model1 = array([1,78]).reshape(-1,1)
+exams.partialRegressions[0].predict(x0model1)
+print('Model 2 Prediction:')
+x0model2 = array([1,85]).reshape(-1,1)
+exams.partialRegressions[1].predict(x0model2)
+print('Model 3 Prediction:')
+x0model3 = array([1,78,85]).reshape(-1,1)
+exams.predict(x0model3)
+
+
+pdb.set_trace()
 
 
 ###############################################################################
@@ -63,17 +76,36 @@ cig.dataFile = '../data/P081.txt'
 #regress full model
 cig.regress(rowLabelCol=0,yCol=7)
 #regress without female
-# cig.partialRegress([5],rowLabelCol=0,yCol=7)
-# cig.partialRegress([2,5],rowLabelCol=0,yCol=7)
-# cig.partialRegress([3],rowLabelCol=0,yCol=7)
-# cig.partialRegress([2,4,5],rowLabelCol=0,yCol=7)
-# cig.partialRegress([1,2,4,5,6],rowLabelCol=0,yCol=7)
-cig.partialRegress([2,3,4,5,6],rowLabelCol=0,yCol=7)
-cig.partialRegress([1,3,4,5,6],rowLabelCol=0,yCol=7)
-cig.partialRegress([1,2,4,5,6],rowLabelCol=0,yCol=7)
-cig.partialRegress([1,2,3,5,6],rowLabelCol=0,yCol=7)
-cig.partialRegress([1,2,3,4,6],rowLabelCol=0,yCol=7)
+cig.partialRegress([5],rowLabelCol=0,yCol=7)
+cig.partialRegress([2,5],rowLabelCol=0,yCol=7)
+cig.partialRegress([3],rowLabelCol=0,yCol=7)
+cig.partialRegress([2,4,5],rowLabelCol=0,yCol=7)
 cig.partialRegress([1,2,3,4,5],rowLabelCol=0,yCol=7)
+
+noFemalePR = cig.partialRegressions[0]
+noFemaleNoHSind = cig.partialRegressions[1]
+print('Problem 3.14')
+print('Part A')
+print('SSEFM: ' + str(cig.SSE))
+print('SSERM: ' + str(noFemalePR.SSE))
+print('n: ' + str(cig.Y.shape[0]))
+print('p: ' + str(cig.X.shape[1]))
+print('k: ' + str(noFemalePR.X.shape[1]))
+print('F: ' + str(cig.fTest[0]))
+print('Part B')
+print('SSERM: ' + str(noFemaleNoHSind.SSE))
+print('SSEFM: ' + str(cig.SSE))
+print('n: ' + str(cig.Y.shape[0]))
+print('p: ' + str(cig.X.shape[1]))
+print('k: ' + str(noFemaleNoHSind.X.shape[1]))
+print('F: ' + str(cig.fTest[1]))
+print('Part C')
+print('beta1Hat' + str(noFemaleNoHSind.betaHat[1]))
+print('seBeta1Hat' + str(noFemaleNoHSind.seBetaJ[1]))
+print('Part d')
+print('rSq, no Income: ' + str(cig.partialRegressions[2].rSq))
+print('rSq, only ' +str(cig.partialRegressions[3].dataNames) + ': ' + str(cig.partialRegressions[3].rSq))
+print('rSq, only ' +str(cig.partialRegressions[4].dataNames) + ': ' + str(cig.partialRegressions[4].rSq))
 
 cig.plotMatrix()
 pdb.set_trace()
@@ -90,6 +122,7 @@ cows.dataFile = '../data/P004.txt'
 cows.regress()
 cows.plotResidualsVPredictors((3,2))
 cows.plotResidualsVFitted()
+cows.plotNormalProb()
 # cows.plotMatrix()
 plt.show()
 pdb.set_trace()
